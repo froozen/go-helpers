31ac3c07df5782e115f36f0ff42409552e1970d9
fro_ozen <fro_ozen@gmx.de>
2014-12-30 22:39:42 +0100

    Add RetrievePageCookies

--- http.go
+++ http.go
@@ -23,3 +23,36 @@ func RetrievePage(url string) (string, error) {
 
 	return string(content), nil
 }
+
+// RetrievePageCookies does the same as RetrievePage, but adds cookies to
+// the http request.
+func RetrievePageCookies(url string, cookies []*http.Cookie) (string, error) {
+	// Crete the client
+	client := &http.Client{}
+
+	// Create the request
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	// Add the cookies
+	for _, cookie := range cookies {
+		request.AddCookie(cookie)
+	}
+
+	// Send the request
+	page, err := client.Do(request)
+	if err != nil {
+		return "", err
+	}
+	defer page.Body.Close()
+
+	// Read the whole page
+	content, err := ioutil.ReadAll(page.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+}
